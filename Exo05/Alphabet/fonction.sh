246813a751fabfd9c2c99336b0251dc6ac05301a
Elliot <gasserelliot02@gmail.com>
2024-01-08 17:34:45 +0100

    add

---
+++ Exo05/Alphabet/fonction.sh
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        ft_print_alphabet();
+    }
+
+    static void ft_print_alphabet()
+    {
+        char lettre = 'a';
+
+        while (lettre <= 'z')
+        {
+            Console.Write(lettre);
+            lettre++;
+        }
+
+        Console.WriteLine();
+    }
+}
